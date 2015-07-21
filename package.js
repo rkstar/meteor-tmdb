1e327381994b3aff9391434f127f26da6b550129
David Fudge <rkstar@mac.com>
2015-07-21 02:14:48 -0400

    updated git url

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'A wrapper for the TMDB (TheMovieDataBase.org) API (v3)',
   // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/rkstar/tmdb',
+  git: 'https://github.com/rkstar/meteor-tmdb',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
