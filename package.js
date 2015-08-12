771a1919f524314ed4117a680a0878bea9131030
David Fudge <rkstar@mac.com>
2015-08-12 18:15:50 -0400

    made config on client into a reactive-dict.  works now.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'rkstar:tmdb',
-  version: '1.1.1',
+  version: '1.1.2',
   // Brief, one-line summary of the package.
   summary: 'A wrapper for the TMDB (TheMovieDataBase.org) API (v3)',
   // URL to the Git repository containing the source code for this package.
@@ -16,11 +16,12 @@ Package.onUse(function(api) {
   api.use('underscore', 'server')
   api.use('http', 'server')
   api.use('service-configuration', 'server')
+  api.use('reactive-dict', 'client')
 
   api.addFiles('tmdb-publication.js', 'server')
   api.addFiles('tmdb-server.js', 'server')
   api.addFiles('tmdb-client.js', 'client')
 
-  api.export("TMDBConfig")
+  api.export("tmdbClient")
   api.export("TMDB")
 })
--- tmdb-client.js
+++ tmdb-client.js
@@ -1,8 +1,10 @@
-TMDBConfig = {}
+tmdbClient = new ReactiveDict('tmdbClient')
+tmdbClient.set('config', {})
+
 Meteor.startup(function(){
   var tmdbConfig = new Mongo.Collection('tmdb-config')
   Meteor.subscribe('tmdb-config')
   Tracker.autorun(function(){
-    TMDBConfig = tmdbConfig.findOne()
+    tmdbClient.set('config', tmdbConfig.findOne())
   })
 })
