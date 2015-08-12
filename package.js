40f859be7f6350119bca754e270d8051053ef3f2
David Fudge <rkstar@mac.com>
2015-08-12 17:46:28 -0400

    updated to use publication

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'rkstar:tmdb',
-  version: '1.1.0',
+  version: '1.1.1',
   // Brief, one-line summary of the package.
   summary: 'A wrapper for the TMDB (TheMovieDataBase.org) API (v3)',
   // URL to the Git repository containing the source code for this package.
@@ -17,11 +17,10 @@ Package.onUse(function(api) {
   api.use('http', 'server')
   api.use('service-configuration', 'server')
 
-  api.addFiles('tmdb-common.js')
-  api.addFiles('tmdb-client.js', 'client')
   api.addFiles('tmdb-publication.js', 'server')
-  api.addFiles('tmdb.js', 'server')
+  api.addFiles('tmdb-server.js', 'server')
+  api.addFiles('tmdb-client.js', 'client')
 
-  api.export("tmdb")
+  api.export("TMDBConfig")
   api.export("TMDB")
 })
--- tmdb-client.js
+++ tmdb-client.js
@@ -1,7 +1,8 @@
+TMDBConfig = {}
 Meteor.startup(function(){
   var tmdbConfig = new Mongo.Collection('tmdb-config')
   Meteor.subscribe('tmdb-config')
   Tracker.autorun(function(){
-    tmdb.config = tmdbConfig.findOne()
+    TMDBConfig = tmdbConfig.findOne()
   })
 })
