291707e8bbc15c6122b70e3ee0a3d0eaf39c4db6
David Fudge <rkstar@mac.com>
2015-10-08 15:21:05 -0400

    added imagePath function to the tmdbClient var to facilitate getting image paths easily.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'rkstar:tmdb',
-  version: '1.2.0',
+  version: '1.3.0',
   // Brief, one-line summary of the package.
   summary: 'A wrapper for the TMDB (TheMovieDataBase.org) API (v3)',
   // URL to the Git repository containing the source code for this package.
