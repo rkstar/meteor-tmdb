530c69ca9cfa564399b7a12b10cee118a515b6cd
David Fudge <rkstar@mac.com>
2015-10-10 21:49:55 -0400

    updated for meteor 1.2.0.2. moved from callbacks to promise-based layout.  refactored to use TMDB.API and TMDB.Client for consistency across the package

--- tmdb-server.js
+++ tmdb-server.js
@@ -1,30 +1,29 @@
-TMDB = function(defaults){
+class API {
+  constructor(defaults={}){
+    // cache the /configuration from tmdb for 3 days before reloading
+    // this can be overridden by passing
+    // {reload:true} or {reloadInterval: moment().subtract('1','day').toDate()} into 'defaults'
+    this.defaults = defaults
+    _.defaults(this.defaults,{
+      reload: false,
+      reloadInterval: moment().subtract('3','days').toDate()
+    })
+    this.defaults.apiUrl = 'https://api.themoviedb.org/3/'
 
-  // constructor ////////////////////////
-  defaults = defaults || {}
-  defaults.apiUrl = 'https://api.themoviedb.org/3/'
+    this.config = ServiceConfiguration.configurations.findOne({service: 'tmdb'})
+    if( !this.config ){
+      throw new ServiceConfiguration.ConfigError('TMDB not configured')
+    }
 
-  // cache the /configuration from tmdb for 3 days before reloading
-  // this can be overridden by passing
-  // {reload:true} or {reloadInterval: moment().subtract('1','day').toDate()} into 'defaults'
-  _.defaults(defaults,{
-    reload: false,
-    reloadInterval: moment().subtract('3','days').toDate()
-  })
-
-  // get the default config for this service from ServiceConfiguration
-  this.config = ServiceConfiguration.configurations.findOne({service: 'tmdb'})
-  if( !this.config ){
-    throw new ServiceConfiguration.ConfigError('TMDB not configured')
-  }
-
-  this.init = function(){
-    if( defaults.reload || !this.config.data || !this.config.loaded || (this.config.loaded < defaults.reloadInterval) ){
+    if( this.defaults.reload
+      || !this.config.data
+      || !this.config.loaded
+      || (this.config.loaded < this.defaults.reloadInterval) ){
       this.reload()
     }
   }
 
-  this.reload = function(){
+  reload(){
     var response = HTTP.call('GET', defaults.apiUrl+'configuration',{params:{api_key: this.config.apiKey}})
     if( response.statusCode == 200 ){
       ServiceConfiguration.configurations.update(
@@ -37,123 +36,129 @@ TMDB = function(defaults){
     }
   }
 
-  this.execute = function(options, callback){
-    if( typeof options === 'string' ){
-      var url = options
-      options = {url: url}
-    }
-    _.defaults(options, {
-      url: '',
-      params: {},
-      method: 'GET'
-    })
-    _.defaults(options.params, {api_key: this.config.apiKey})
-
-    if( !options.url || (options.url.length < 1) ){
-      callback(new Meteor.Error(500, 'No path to TMDB API provided.'))
-      return
-    }
-
-    if( options.url.indexOf(defaults.apiUrl) != 0 ){
-      options.url = defaults.apiUrl + options.url
-    }
-
-    HTTP.call(options.method, options.url, {params: options.params}, function(err, response){
-      if( response.statusCode != 200 ){
-        callback(new Meteor.Error(response.data.status_code, response.data.status_message))
-      } else {
-        callback(null, response.data)
+  execute(opts){
+    return new Promise((resolve, reject)=>{
+      if( _.isString(opts) ){
+        let url = opts
+        opts = {url}
       }
+      _.defaults(opts,{
+        url: '',
+        params: {},
+        method: 'GET'
+      })
+      _.defaults(opts.params, {api_key: this.config.apiKey})
+
+      if( !opts.url || (opts.url.length < 1) ){
+        reject(new Meteor.Error(500, 'No path to TMDB API provided'))
+        return
+      }
+
+      HTTP.call(opts.method, this.defaults.apiUrl+opts.url, {params: opts.params}, (err, response)=>{
+        if( response.statusCode != 200 ){
+          reject(new Meteor.Error(response.data.status_code, response.data.status_message))
+        } else {
+          resolve(response.data)
+        }
+      })
     })
   }
 
-  this.search = function(path, query, callback){
-    if( !path || (path.length < 1) ){
-      callback(new Meteor.Error(429, 'Nothing to search.'))
-      return
-    }
-    if( !query || (query.length < 1) ){
-      callback(new Meteor.Error(429, 'No query specified.'))
-      return
-    }
+  search(path, query){
+      if( !path || (path.length < 1) ){
+        return new Promise((resolve, reject)=>{
+          reject(new Meteor.Error(429, 'Nothing to search'))
+        })
+      }
+      if( !query || (query.length < 1) ){
+        return new Promise((resolve, reject)=>{
+          reject( new Meteor.Error(429, 'No query specified'))
+        })
+      }
 
-    this.execute({
-      url: 'search/'+path,
-      params: {query: query}
-    }, callback)
+      return this.execute({
+        url: `search/${path}`,
+        params: {query}
+      })
   }
 
-  this.find = function(id, source, callback){
-    if( !callback && (typeof source == 'function') ){
-      callback = source
-      source = 'IMDB'
-    }
+  findById(id, source='imdb'){
     if( !id || (id.length < 1) ){
-      callback(new Meteor.Error(500, 'No IMDB ID specified.'))
-      return
+      return new Promise((resolve, reject)=>{
+        reject(new Meteor.Error(500, 'No ID specified'))
+      })
     }
 
-    source = source.replace(/\s+/g,'')
+    // replace spaces in the source name
+    source = source.replace(/\s+/g, '')
     switch( source.toLowerCase() ){
       case 'freebase/movie':
         source += '_mid'
         break
 
+      case 'imdb':
       default:
         source += '_id'
+        break
     }
 
-    this.execute({
-      url: 'find/'+id,
-      params: {external_source: source}
-    }, function(err, response){
-      if( err ){
-        callback(err)
-      } else {
-        // format the results a little nicer for our end users
-        callback(null,{
-          movies: response.movie_results,
-          people: response.person_results,
-          tv: {
-            shows: response.tv_results,
-            seasons: response.tv_season_results,
-            episodes: response.tv_episode_results
-          }
-        })
-      }
+    return new Promise((resolve, reject)=>{
+      this.execute({
+        url: `find/${id}`,
+        params: {external_source: source}
+      })
+      .then((result)=>{
+          resolve({
+            movies: result.movie_results,
+            people: result.person_results,
+            tv: {
+              shows: result.tv_results,
+              seasons: result.tv_season_results,
+              episodes: result.tv_episode_results
+            }
+          })
+        }, (err)=>{ reject(err) })
     })
   }
 
-  this.specific = function(url, err, id, search, callback){
-    if( !callback && (typeof search == 'function') ){
-      callback = search
-      search = null
+  specific(url, err, id, search){
+    let id_required = !(search && (['popular','latest'].indexOf(search) != -1))
+    if( (!id || (id.length < 1)) && id_required ){
+      return new Promise((resolve, reject)=>{
+        reject(new Meteor.Error(500, err))
+      })
     }
 
-    var id_required = !(search && (['popular','latest'].indexOf(search) != -1))
-    if( !id && id_required ){
-      callback(new Meteor.Error(500, err))
-      return
-    }
-    url += (id_required) ? '/'+id : ''
-    url += (search) ? '/'+search : ''
-    this.execute({
-      url: url
-    }, callback)
+    url += (id_required) ? `/${id}` : ''
+    url += (search) ? `/${search}` : ''
+    return this.execute({url})
   }
 
-  this.person = function(id, search, callback){
-    this.specific('person', 'No profile id specified', id, search, callback)
+  person(id, search){
+    return this.specific('person', 'No profile ID specified', id, search)
   }
 
-  this.tv = function(id, search, callback){
-    this.specific('tv', 'No tv id specified', id, search, callback)
+  tv(id, search){
+    return this.specific('tv', 'No TV ID specified', id, search)
   }
 
-  this.movie = function(id, search, callback){
-    this.specific('movie', 'No movied id specified', id, search, callback)
+  movies(id, search){
+    return this.specific('movie', 'No movie ID specified', id, search)
   }
 
-  // this must be called after all functions are declared above
-  this.init()
+  get config(){
+    return this._config
+  }
+  set config(value){
+    this._config = value
+  }
+
+  get defaults(){
+    return this._defaults
+  }
+  set defaults(value){
+    this._defaults = value
+  }
 }
+
+TMDB.API = API
