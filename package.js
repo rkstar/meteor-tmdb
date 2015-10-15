29da0bb4ac2bfac5e68fb58c934be7400d3ebbf3
David Fudge <rkstar@mac.com>
2015-10-15 14:15:38 -0400

    version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'rkstar:tmdb',
-  version: '2.0.0',
+  version: '2.0.2',
   // Brief, one-line summary of the package.
   summary: 'A wrapper for the TMDB (TheMovieDataBase.org) API (v3)',
   // URL to the Git repository containing the source code for this package.
